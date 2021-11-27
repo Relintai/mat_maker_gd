3afa4a93b5c825db84d1eac327174f9510041591
Relintai <relintai@protonmail.com>
2021-11-27 17:46:42 +0100

    Bring back the addon from the broken seals repo.

--- material_maker/nodes/debug/debug_popup.gd
+++
@@ -1,6 +0,0 @@
-extends Popup
-
-func show_code(text : String) -> void:
-	$TextEdit.text = text
-	connect("popup_hide", self, "queue_free")
-	popup_centered()
--- material_maker/panels/preview_3d/preview_3d_ui.gd
+++
@@ -1,7 +0,0 @@
-extends HBoxContainer
-
-export var target : NodePath = ".."
-var target_node
-
-func _ready() -> void:
-	target_node = get_node(target)
